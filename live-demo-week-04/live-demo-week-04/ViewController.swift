8fce712062ab46cc277aa271445baa4f5fcb8499
Alejandro Guerrero <Gu_Alejandro.007@csu.fullerton.edu>
2023-09-12 16:50:10 -0700

    call back links for buttons

--- live-demo-week-04/live-demo-week-04/ViewController.swift
+++ live-demo-week-04/live-demo-week-04/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet  var button1: UIButton!
+    @IBOutlet  var button2: UIButton!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var textEdit: UITextField!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var mySwitch: UISwitch!
+    @IBOutlet var progress: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func printButtonLabel(_ btn: UIButton) {
+        
+    }
+    
+    @IBAction func buttonTapped(_ btn: UIButton) {
+        print("Button tapped!")
+        self.printButtonLabel(btn)
+    }
 
+    @IBAction func buttonDragged(_ btn: UIButton) {
+        print("Button tapped!")
+        self.printButtonLabel(btn)
+        }
+        
 
+    
+    @IBAction func segmentedControlChanged(_ c: UISegmentedControl) {
+        print("Semented control changed!")
+    }
+    
+    @IBAction func textEditChanged(_ t: UITextField) {
+        
+        if let text = t.text {
+            
+            print("Text field changed!")
+        } else {
+            print("Text field changed, but the text was null!")
+        }
+    }
+    
 }
+
 
